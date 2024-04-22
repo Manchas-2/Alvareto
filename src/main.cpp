8f1ec9edd62d5f6099eda32757989ffa18fc79f8
Manchas-2 <167267501+Manchas-2@users.noreply.github.com>
2024-04-21 20:40:59 -0700

    lestura_DS18B20
    funciones de lectura 
    cambio de definicón de lectura 
    ¿Confirmación de que el codigo copile?

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,29 @@
 #include <Arduino.h>
-
+#include <OneWire.h> // libreria"OneWire"para el sensor digital
+#include <DallasTemperature.h> // libreria "Dallas Temperature" para converción de la señal 
 #define mn   2
 #define ent  3   // funcionnamiento manual o app
-#define sen  4   //Sensor de Temperatura
 #define bos  5   //Bosina 
 #define ret  6   //Rele resistencia
 #define dis  7   //Rele disipador 
+OneWire ourWire(4);   //Sensor de temperatura
+DallasTemperature sensors(&ourWire);float mx = 32.9;
+float temp; //temperatra real 
 float mx = 34;
 int fn = 0;
-int pinLed=2;
-int n=2,c,d,i ;
+;
 
 void setup() {
 Serial.begin (9600);
+sensors.begin();
+delay (1000);
 pinMode  (mn   , INPUT );
 pinMode  (ent  , INPUT );
 pinMode  (sen  , INPUT );
 pinMode  (bos  , OUTPUT );
 pinMode  (ret  , OUTPUT );
 pinMode  (dis  , OUTPUT );
-pinMode  (pinLed, OUTPUT);
 Serial.print ("Ensendido:");
-c=5;
 }
 void loop() {
  if (digitalRead( ent) == HIGH ) //Definir como va a funcionar manual o app
@@ -32,17 +34,24 @@ void loop() {
   {
     fn = 2 ;
   }
+sensors.requestTemperatures();   //Se envía el comando para leer la temperatura
+temp= sensors.getTempCByIndex(0); // se almacena la temperaura
 switch (fn){
  case 1 : // Funcionamiento manual 
   while (sen < mx )   //funciona hasta que la sen no supere la temperatura 
-  { digitalWrite ( ret , HIGH );
+  { Serial.print("Temperatura= ")
+    Serial.print(temp);
+    Serial.println(" C");
+    digitalWrite ( ret , HIGH );
     delay (3500);
     digitalWrite ( ret , LOW );
     delay (1500);
     Serial.println("Claentando...");
   }
   while(sen > mx ) //estado de espera 
-  {
+  { Serial.print("Temperatura= ");
+    Serial.print(temp);
+    Serial.println(" C");
     digitalWrite ( dis , HIGH );
     delay (2500);
     digitalWrite ( dis , LOW );
@@ -51,21 +60,13 @@ switch (fn){
   break;
  case 2 :  // Fucinamiento Remoto 
   Serial.print("Remoto");
+
+  
   break;
  default: // Error de funcionamiento 
- Serial.print("Error");
+  Serial.print("Error");
  
- return;// salir del  selector 
- break;
-}
-  n += 3;
-  c += 2,
-
-  digitalWrite (pinLed, HIGH);
-   Serial.print ("ON, ");
-   delay (1000);
-  
-  digitalWrite (pinLed, LOW);
-   Serial.println ("OFF");
-   delay(1000);
+  return;// salir del  selector 
+  break;
+ }
 }
